e0d586f190c067a9b7281ead29e4f36a2669d0b9
man-2005 <manojnagraj18@gmail.com>
2025-07-23 20:02:49 +0530

    encapsulation concept

---
+++ Person.java
@@ -0,0 +1,59 @@
+package com.tnsif.encapsulation;
+
+public class Person {
+	private String name;
+	private int income;
+	private String gender;
+	private int age;
+	private int tax;
+	
+	//this is constructors generated for data members 
+	//because for more data members setter and getter will be complex
+	public Person(String name, int income, String gender, int age) {
+		//super();
+		this.name = name;
+		this.income = income;
+		this.gender = gender;
+		this.age = age;
+	}
+	
+	//getter and setter methods
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getIncome() {
+		return income;
+	}
+	public void setIncome(int income) {
+		this.income = income;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getTax() {
+		return tax;
+	}
+	public void setTax(int tax) {
+		this.tax = tax;
+	}
+	@Override
+	public String toString() {
+		return "Person [name=" + name + ", income=" + income + ", gender=" + gender + ", age=" + age + ", tax=" + tax
+				+ "]";
+	}
+	
+	
+
+}
---
+++ PersonDemo.java
@@ -0,0 +1,20 @@
+package com.tnsif.encapsulation;
+
+public class PersonDemo {
+	public static void main(String args[]) {
+		
+//	Person obj=new Person();
+//	obj.setAge(22);
+//	obj.setGender("female");
+//	obj.setIncome(20000);
+//	obj.setName("roopa");
+//	//so now if we have so many data getter and setter will be complex because everytime
+//	//setting and getting values in main class will be complex
+//	//so constructors can be used.
+	
+	Person p1=new Person("Roopa",20000,"female",22);
+	
+	
+
+}
+}
