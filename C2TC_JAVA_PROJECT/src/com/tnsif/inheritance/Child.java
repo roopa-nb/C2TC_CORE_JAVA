f70b9a9990477ee4e37311b55c8aee202504e4ca
man-2005 <manojnagraj18@gmail.com>
2025-07-28 19:41:51 +0530

    inheritance

---
+++ C2TC_JAVA_PROJECT/src/com/tnsif/inheritance/Child.java
@@ -0,0 +1,15 @@
+package com.tnsif.inheritance;
+
+public class Child extends Father{
+
+	public static void main(String args[]) {
+		
+		Child ch = new Child(); // normally we were creating obj for father where properties and behaviours were there
+		//but in inheritance concept we should create obj of inherited class
+		ch.coffee();
+		ch.tea();
+		System.out.println(ch.colour);
+		System.out.println(ch.money);
+		
+	}
+}
---
+++ C2TC_JAVA_PROJECT/src/com/tnsif/inheritance/Father.java
@@ -0,0 +1,17 @@
+package com.tnsif.inheritance;
+
+public class Father {
+	
+	int money=8000;
+	String colour= "pink";
+	
+	public void coffee() {
+		System.out.println("i'm doing coffee");
+	}
+	
+	public void tea() {
+		System.out.println("i'm drinking tea");
+	}
+
+
+	}
